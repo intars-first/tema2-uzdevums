76ec1583f566dda899524d333a3ff8ac4c221d74
Intars Kampuss <intars.kampuss@gmail.com>
2021-09-07 21:45:07 +0300

    uzdevuma primā daļa

---
+++ lesson2-uzdevums/lesson2-uzdevums/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lesson2_uzdevums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kā tevi sauc?");
+            string userAnswer = Console.ReadLine();
+            Console.WriteLine("Sveiks, " + userAnswer);
+
+            Console.WriteLine("=======================================================");
+
+
+        }
+    }
+}
